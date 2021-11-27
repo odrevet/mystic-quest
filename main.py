1c086a4a41bb8a0bc4b8b4b2a584705180823cac
odrevet <odrevet@gmail.com>
2021-11-27 20:49:20 +0100

    q to quit; check map boundaries

--- main.py
+++ main.py
@@ -3,29 +3,30 @@ import pytmx
 
 pygame.init()
 
-
 display_width = 160
 display_height = 144
-
 screen = pygame.display.set_mode((display_width, display_height))
+
 pygame.display.set_caption("Mystic Quest")
 clock = pygame.time.Clock()
 
-# load map data
 tm = pytmx.load_pygame("en/mapas/mapa_00_00.tmx")
 
+MAP_WIDTH = 10
+MAP_HEIGHT = 8
+
+map_index_x = 0
+map_index_y = 0
 
 def main():
-
-    if __debug__:
-        for visible_layer in tm.visible_layers:
-            print(visible_layer)
-
     done = False
     while not done:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    done = True
 
         tile_layer = tm.get_layer_by_name("Tile Layer 1")
         for (
@@ -33,8 +34,9 @@ def main():
             y,
             gid,
         ) in tile_layer:
-            tile = tm.get_tile_image_by_gid(gid)
-            screen.blit(tile, (x * tm.tilewidth, y * tm.tileheight))
+            if x < MAP_WIDTH and y < MAP_HEIGHT:
+                tile = tm.get_tile_image_by_gid(gid)
+                screen.blit(tile, (x * tm.tilewidth, y * tm.tileheight))
 
         pygame.display.update()
         clock.tick(30)
