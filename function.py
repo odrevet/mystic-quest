e9e86c7e22886eba739fee92e19c37df77cb8ea4
Olivier Drevet <odrevet@gmail.com>
2021-12-12 08:33:49 +0100

    fix teleport location

--- function.py
+++ function.py
@@ -3,8 +3,9 @@ def MUSIC(game, id):
 
 def TELEPORT(game, MM, BB, XX, YY):
     print(f"Teleport to {MM} {BB} {XX} {YY}")
-    game.hero.x = int(XX, 16)
-    game.hero.y = int(YY, 16)
+    game.hero.x = int(XX, 16) * 8
+    game.hero.y = int(YY, 16) * 8
+    block_number = int(YY, BB)
     game.map_screen_index_x = 0 # TODO from BB
     game.map_screen_index_y = 0
     game.load_map(MM)
